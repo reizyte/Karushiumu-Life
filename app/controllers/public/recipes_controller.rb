511ed75d2fc3520a6935b1a3aa66173f0ce11bbb
reizyte <112692614+reizyte@users.noreply.github.com>
2022-11-05 18:47:39 +0000

    レシピ登録フォーム着手

--- app/controllers/public/recipes_controller.rb
+++ app/controllers/public/recipes_controller.rb
@@ -1,2 +1,32 @@
 class Public::RecipesController < ApplicationController
+
+  def new
+    @recipe = Recipe.new
+    @cooking_materials = @recipe.cooking_materials.build  #親モデル.子モデル.buildで子モデルのインスタンス作成
+    @how_to_makes = @recipe.how_to_makes.build
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.save
+    redirect_to recipe_path(@recipe)
+  end
+
+  def destroy
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:dish_name, :explanation, :cooking_time, :serving, :image,
+                                  recipe_materials_attributes:[:material_name, :quantity, :_destroy],
+                                  how_to_makes_attributes:[:cooking_procedure, :_destroy])
+
+  end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,8 +1,10 @@
 class Recipe < ApplicationRecord
+  has_one_attached :image
   belongs_to :customer
   belongs_to :genre
   has_many :favorites, dependent: :destroy
   has_many :recipe_comments, dependent: :destroy
   has_many :how_to_makes, dependent: :destroy
   has_many :cooking_materials, dependent: :destroy
+  accepts_nested_attributes_for :cooking_materials, :how_to_makes, allow_destroy: true
 end
