006e0f05d55539e66b20bec040c1c5242385ee5b
reizyte <112692614+reizyte@users.noreply.github.com>
2022-11-13 18:13:50 +0000

    [add]レイアウト、リンク調整

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -4,13 +4,11 @@ class Public::FavoritesController < ApplicationController
     @recipe = Recipe.find(params[:recipe_id])
     favorite = current_customer.favorites.new(recipe_id: @recipe.id)
     favorite.save
-    #redirect_to request.referer
   end
 
   def destroy
     @recipe = Recipe.find(params[:recipe_id])
     favorite = current_customer.favorites.find_by(recipe_id: @recipe.id)
     favorite.destroy
-    #redirect_to request.referer
   end
 end
