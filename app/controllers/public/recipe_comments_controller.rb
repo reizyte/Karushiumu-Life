ddeb3efbb53d884024e9b042e6ef27ee1685ab19
reizyte <112692614+reizyte@users.noreply.github.com>
2022-11-16 12:05:13 +0000

    [add]コメントの管理者権限付与

--- app/controllers/public/recipe_comments_controller.rb
+++ app/controllers/public/recipe_comments_controller.rb
@@ -1,5 +1,5 @@
 class Public::RecipeCommentsController < ApplicationController
-  before_action :authenticate_customer!
+  before_action :authenticate_customer!, except:[:destroy]
   def create
     recipe = Recipe.find(params[:recipe_id])
     @comment = current_customer.recipe_comments.new(recipe_comment_params)
